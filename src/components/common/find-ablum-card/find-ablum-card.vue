cf6e25f0ac5c4947c11205c284d32509dc6d4cdb
tommy <tommy@find.com>
2018-05-03 18:30:43 +0800

    曲风item ui组件封装

---
+++ src/components/common/find-ablum-card/find-ablum-card.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class='find-ablum-card'>
+    <findAblumTitle></findAblumTitle>
+    <findAlbum :albumArr="ablumArr"></findAlbum>
+  </div>
+</template>
+<style lang=scss scope type=text/scss>
+  .find-ablum-card {
+    width: 800px;
+    height: 330px;
+    background: url("./bg_qupuxilie.png") no-repeat;
+    -webkit-background-size: 100% 100%;
+    background-size: 100% 100%;
+    display: flex;
+    position: relative;
+    ul {
+      position: absolute;
+      height: 246px;
+      right: 70px;
+      top: 42px;
+    }
+    .find-ablum-title {
+      position: absolute;
+      left: 40px;
+      top: 115px;
+    }
+  }
+</style>
+
+<script>
+  import findAlbum from 'components/common/find-Album/find-Album'
+  import findAblumTitle from 'components/common/find-ablum-title/find-ablum-title'
+
+  export default {
+    name: 'popular',
+    props: {
+      ablumArr: Object
+    },
+    data () {
+      return {
+      }
+    },
+    components: {
+      findAlbum,
+      findAblumTitle
+    }
+  }
+</script>
---
+++ src/components/common/find-ablum-title/find-ablum-title.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="find-ablum-title">
+    <p class="name">约翰·汤普森</p>
+    <span class="num"><span>5册</span><span>36篇</span></span>
+  </div>
+</template>
+<style lang="scss" scope type=text/scss>
+  .find-ablum-title {
+    .name {
+      font-size: 36px;
+      color: #334866;
+      border-bottom: 1px solid #597399;
+      padding-bottom: 10px;
+      margin: 0;
+    }
+    .num {
+      padding-top: 10px;
+      padding-right: 70px;
+      padding-left: 5px;
+      display: flex;
+      justify-content: space-between;
+      font-size: 20px;
+      color: #597399;
+    }
+  }
+</style>
+<script type="es6">
+  export default {
+    name: 'find-ablum-title',
+    data () {
+      return {}
+    }
+  }
+</script>
