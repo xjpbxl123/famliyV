349170381b0e9a3214d35572c14c93cc2fa2698d
zx <1147732340@qq.com>
2018-05-04 19:17:58 +0800

    FINDH5-60

--- src/routers/index.js
+++ src/routers/index.js
@@ -10,6 +10,7 @@ import famousBook from 'components/famous/famous-book/famousBook-router'
 import popular from 'components/popular/popular-router'
 import scoreSetList from 'components/scoreSetList/scoreSetList-router'
 import scoreList from 'components/scoreList/scoreList-router'
+
 export default [
   index,
   home,
