a41a4b61f2ff7c4f9e1054ba968b3746a6fe41ca
Moersing <laiyuanjie@findpiano.cn>
2018-05-24 15:23:48 +0800

    What: Fixed WDS HMR
    Why:
    *
    How:
    *

--- src/scripts/eventsHub/index.js
+++ src/scripts/eventsHub/index.js
@@ -1,5 +1,7 @@
 /**
  * Created by moersing on 15/05/2017.
+ * @description EventHub is for communication between parent-to-children and sibling components,
+ * Get more information please visit https://vuejs.org/v2/guide/migration.html#dispatch-and-broadcast-replaced
  */
 import Vue from 'vue'
 export default new Vue()
