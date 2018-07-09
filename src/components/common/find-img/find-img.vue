42b087880d9de476af76774900c222e666baeca4
詹潇 <zhanxiao@zhanxiaodeMacBook-Pro.local>
2018-07-09 14:50:33 +0800

    img:bugfix

--- src/components/common/find-img/find-img.vue
+++ src/components/common/find-img/find-img.vue
@@ -57,6 +57,7 @@
       }
       window.fp.modules.file.cacheUrl(this.src).then(data => {
         if (data.code === 0) {
+          console.log('cacheUrl', data.url)
           let image = new Image()
           image.src = data.url
           image.onload = () => {
@@ -66,7 +67,7 @@
                 this.url = data.url
                 clearInterval(checkoutImg)
               }
-            }, 1000)
+            }, Math.random() * 1000)
           }
         } else {
           console.log(data.desc)
